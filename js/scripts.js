4c629ea18bfa4da753099f52e48a6cb3f6a21419
Larry Taylor and Max Scher <larry@>
2017-06-01 15:30:47 -0700

    add jquery elements to get political result

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
-$(document).ready(function(){
-  event.preventDefault();
-  $("form#politics").submit(function(event){
-    var environment = parseInt("input#environment").val();
-    var gun_control = parseInt("input#gun_control").val();
-    var abortion = parseInt("input#abortion").val();
-    var welfare = parseInt("input#welfare").val();
+$(document).ready(function() {
+  $("#politics").submit(function(event){
+    event.preventDefault();
 
-    if ((environment + abortion) > (gun_control + welfare)){
-      $("#slant")
+    var environment = parseInt($("input#environment").val());
+    var gun_control = parseInt($("input#gun_control").val());
+    var abortion = parseInt($("input#abortion").val());
+    var welfare = parseInt($("input#welfare").val());
+
+    if ((environment + abortion) > (gun_control + welfare)) {
+      $("#liberal").show();
+    } else {
+      $("#conservative").show();
     }
-
-  })
+  });
 });
